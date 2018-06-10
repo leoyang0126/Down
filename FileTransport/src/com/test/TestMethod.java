33549dcbc3949268a2057b26bcb0e9ed46c454d4
yangliu <leoyang0126@163.com>
2018-06-10 18:32:49 +0800

    update thread

--- FileTransport/src/com/test/TestMethod.java
+++ FileTransport/src/com/test/TestMethod.java
@@ -9,7 +9,7 @@ public class TestMethod {
     public TestMethod() {
         try {
             DownFileInfoBean bean = new DownFileInfoBean(
-                    "http://sw.bos.baidu.com/sw-search-sp/software/e335feb5c2f01/WeChatSetup.exe", "/Users/yangliu/Documents/temp",
+                    "http://sw.bos.baidu.com/sw-search-sp/software/e335feb5c2f01/WeChatSetup.exe", "/Users/Documents/temp",
                     "WeChatSetup.exe", CUPNUM*2, true, null);  //http://dlsw.baidu.com/sw-search-sp/soft/a8/27390/androidstudio1.5.0.0.1454148047.exe
             /*File file = new File("D:\\dan07.apk"); http://dlsw.baidu.com/sw-search-sp/soft/0c/20336/ShengSangokushiEiketsuden_1.0.5.6.2836385435.exe
              * http://dlsw.baidu.com/sw-search-sp/soft/34/21499/WindFantasy4_1.0.2212294583.exe
